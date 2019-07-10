0e6afaa8d8705b8fc60b1f29aeba8f9e9a8428b8
Dzhan85 <Dzhan85@users.noreply.github.com>
2019-07-10 12:59:11 +0300

    Update task_4_1.py

--- exercises/04_data_structures/task_4_1.py
+++ exercises/04_data_structures/task_4_1.py
@@ -10,3 +10,9 @@
 '''
 
 nat = 'ip nat inside source list ACL interface FastEthernet0/1 overload'
+nat.replace('Fast','Gigabit')
+
+or
+
+nat = 'ip nat inside source list ACL interface FastEthernet0/1 overload'
+print nat.replace('Fast','Gigabit')

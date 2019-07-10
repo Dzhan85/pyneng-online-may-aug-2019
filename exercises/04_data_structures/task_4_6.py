be511c8520131cc4ec0e450658a8295c81c07d80
Dzhan85 <Dzhan85@users.noreply.github.com>
2019-07-10 16:00:02 +0300

    Update task_4_6.py

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -14,5 +14,32 @@ Outbound Interface     FastEthernet0/0
 
 '''
 
-ospf_route = 'OSPF      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
+ospf_route = 'O     10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
+ospf_route.replace('O, 'OSPF')
 str1 = ospf_route.strip().split()
+str1.remove('via')
+ names = (
+    ...: 'Protocol',
+    ...: 'Prefix'
+    ...: 'AD/Metric',
+    ...: 'Next-Hop',
+    ...: 'Last-Update',
+    ...: 'Outbound Interface')
+
+                   
+
+                   
+fields = tuple(i + ':' for i in names)
+
+str1 = re.sub(r',|via| [\[\]]', '', text).split()
+lst = zip(fields, str1)
+width = max(map(len,fields))
+
+format = '{:%d}{:10}' % (width + 5)
+
+result = '\n'.join(format.format(*i) for i in lst)
+
+print result
+
+                   
+                 

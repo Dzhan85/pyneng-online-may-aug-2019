6c810c5113382e0cd38ea6b7cecc19965ae630de
Dzhan85 <Dzhan85@users.noreply.github.com>
2019-07-10 15:27:27 +0300

    Update task_4_6.py

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -14,4 +14,5 @@ Outbound Interface     FastEthernet0/0
 
 '''
 
-ospf_route = 'O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
+ospf_route = 'OSPF      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
+str1 = ospf_route.strip().split()
